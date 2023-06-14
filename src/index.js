29f943f4250d33eb5387ff2007054d832e09c7a0
유민지 <yyoumi4854@gmail.com>
2023-06-14 17:15:05 +0900

    바닐라 자바스크립으로 counter 구현

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+const add = document.getElementById("add");
+const minus = document.getElementById("minus");
+const number = document.querySelector("span");
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+let count = 0;
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const updateText = () => {
+  number.innerText = count;
+};
+
+const hanleAdd = () => {
+  count += 1;
+  updateText();
+};
+
+const hanleMinus = () => {
+  count -= 1;
+  updateText();
+};
+
+add.addEventListener("click", hanleAdd);
+minus.addEventListener("click", hanleMinus);
