cbcbea15290616fae0014f90feda4e556052d214
유민지 <yyoumi4854@gmail.com>
2023-06-16 15:36:36 +0900

    자바스크립트 + 리덕스로 ToDoList 구현

--- src/index.js
+++ src/index.js
@@ -1,64 +1,81 @@
 import { legacy_createStore } from "redux";
 
-const add = document.getElementById("add");
-const minus = document.getElementById("minus");
-const number = document.querySelector("span");
+const form = document.querySelector("form");
+const input = document.querySelector("input");
+const ul = document.querySelector("ul");
 
-number.innerText = 0;
-
-/**
- * reducer나 modifier는 처음부터 data를 바꿔준다.
- * return하는 값이 application에 있는 data이다.
- *
- * 1. data의 store를 만들고(create하고)
- * 2. data의 countModifier에 message를 store에 젼송하면 된다.
- * 3. meeage를 send하는 방법은 dispatch를 사용하면 된다.
- * 4. message는 action에 넣으면 되고, 우리는 action을 체크해 보면 된다.
- */
-
-const ADD = "ADD";
-const MINUS = "MINUS";
+// action type
+const ADD_TODO = "ADD_TODO";
+const DELETE_TODO = "DELETE_TODO";
 
 /**
- * action : redux에서 function을 부를때 쓰는 두번째 파라미터
- * - countModifier과 소통 하는 방법
+ * 리덕스에서는 mutate를 해주면 안된다. -> 즉 기존 배열을 수정하지말아라
+ * => 새로운 state를 create하고 그 새로운 state를 return해야 된다.
  */
-const countModifier = (count = 0, action) => {
+
+// action 생성
+const addToDo = (text) => {
+  return {
+    type: ADD_TODO,
+    text,
+  };
+};
+const deleteToDo = (id) => {
+  return {
+    type: DELETE_TODO,
+    id,
+  };
+};
+
+const reducer = (state = [], action) => {
   switch (action.type) {
-    case ADD:
-      return count + 1;
-    case MINUS:
-      return count - 1;
+    case ADD_TODO:
+      const newToDoObj = { text: action.text, id: Date.now() };
+      return [...state, newToDoObj];
+    case DELETE_TODO:
+      const cleaned = state.filter((toDo) => toDo.id !== action.id);
+      return cleaned;
     default:
-      return count;
+      return [];
   }
 };
 
-/**
- * legacy_createStore에는
- * dispatch, subscribe, getState등이 4개의 함수가 있다.
- * getState : stoore의 현재 값을 알게 해준다.
- * subscribe : store안에 있는 변화들을 알 수 있게 해준다.
- */
-const countStore = legacy_createStore(countModifier);
+const store = legacy_createStore(reducer);
+store.subscribe(() => console.log(store.getState()));
 
-const onChange = () => {
-  number.innerText = countStore.getState();
+// dispatch
+const dispatchAddToDo = (text) => {
+  store.dispatch(addToDo(text));
+};
+const dispatchDeleteToDo = (e) => {
+  const id = parseInt(e.target.parentNode.id);
+  store.dispatch(deleteToDo(id));
 };
 
-countStore.subscribe(onChange);
+const paintToDos = () => {
+  const toDos = store.getState();
+  ul.innerHTML = "";
+  toDos.forEach((toDo) => {
+    const li = document.createElement("li");
+    const btn = document.createElement("button");
 
-/**
- * countStore.dispatch()을 입력해서 action을 보낼 수 있다.
- * - 메시지는 object로 넣어줘야 된다.
- * - 무조건 type이여야 된다.
- */
-const handleAdd = () => {
-  countStore.dispatch({ type: ADD });
+    btn.innerText = "DEL";
+    btn.addEventListener("click", dispatchDeleteToDo);
+
+    li.id = toDo.id;
+    li.innerText = toDo.text;
+
+    li.appendChild(btn);
+    ul.appendChild(li);
+  });
 };
-const handleMinus = () => {
-  countStore.dispatch({ type: MINUS });
+store.subscribe(paintToDos);
+
+const onSubmit = (e) => {
+  e.preventDefault();
+  const toDo = input.value;
+  input.value = "";
+  dispatchAddToDo(toDo);
 };
 
-add.addEventListener("click", handleAdd);
-minus.addEventListener("click", handleMinus);
+form.addEventListener("submit", onSubmit);
